962b900f4db9f1c6791279b81dcb7583ab318823
vuteam <rakzami39@gmail.com>
2016-11-17 05:31:03 +0100

    first commit

---
+++ lib/base/cfile.h
@@ -0,0 +1,55 @@
+#ifndef __lib_base_cfile_h
+#define __lib_base_cfile_h
+
+#include <stdio.h>
+#include <string>
+#include <lib/base/eerror.h>
+
+/* Wrapper around FILE to prevent leaks and to make your code a bit more OO */
+struct CFile
+{
+	FILE *handle;
+	CFile(const char *filename, const char *mode)
+		: handle(fopen(filename, mode))
+	{
+#ifdef DEBUG
+		if (!handle)
+		{
+			std::string fn = filename;
+			std::string ext = fn.substr(fn.find_last_of('.')+1);
+			if( (ext != "meta") && (ext != "epl"))
+				eDebug("[CFile] information %s [%m]", filename);
+		}
+#endif
+	}
+	CFile(const std::string &filename, const char *mode)
+		: handle(fopen(filename.c_str(), mode))
+	{
+#ifdef DEBUG
+		if (!handle)
+		{
+			std::string ext=filename.substr(filename.find_last_of('.')+1);
+			if( (ext != "meta") && (ext != "epl"))
+				eDebug("[CFile] information %s [%m]",filename.c_str());
+		}
+#endif
+	}
+	~CFile()
+	{
+		if (handle)
+			fclose(handle);
+	}
+	void sync() { fsync(fileno(handle)); }
+	operator bool() const { return handle != NULL; }
+	operator FILE*() const { return handle; }
+
+	/* Fetch integer from /proc files and such */
+	static int parseIntHex(int *result, const char *filename);
+	static int parseInt(int *result, const char *filename);
+	static int writeIntHex(const char *filename, int value);
+	static int writeInt(const char *filename, int value);
+	static int write(const char *filename, const char *value);
+	static int writeStr(const char *filename, std::string value);
+};
+
+#endif
